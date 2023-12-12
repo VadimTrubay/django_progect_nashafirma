2677526b2c600fecd45a6ce5e23a9b6a95dc3df7
VadimTrubay <vadnetvadnet@ukr.net>
2023-12-12 21:55:09 +0200

    commit 12 12 23

--- nashafirma/users/forms.py
+++ nashafirma/users/forms.py
@@ -26,31 +26,9 @@ class RegistrationForm(UserCreationForm):
             "password2",
         )
 
-    # def __init__(self, *args, **kwargs):
-    #     super(RegistrationForm, self).__init__(*args, **kwargs)
-    #     self.fields["username"].widget.attrs["placeholder"] = "Enter username"
-    #     self.fields["first_name"].widget.attrs["placeholder"] = "Enter first name"
-    #     self.fields["last_name"].widget.attrs["placeholder"] = "Enter last name"
-    #     self.fields["email"].widget.attrs["placeholder"] = "Enter email"
-    #     self.fields["telephone_number"].widget.attrs[
-    #         "placeholder"
-    #     ] = "Enter phone number"
-    #     self.fields["password1"].widget.attrs["placeholder"] = "Enter password"
-    #     self.fields["password2"].widget.attrs["placeholder"] = "Confirm password"
-
-
 class LoginForm(AuthenticationForm):
     captcha = CaptchaField()
-    username = UsernameField(
-        widget=forms.TextInput(attrs={"autofocus": True, "placeholder": "Username"})
-    )
-    password = forms.CharField(
-        strip=False,
-        widget=forms.PasswordInput(
-            attrs={"autocomplete": "current-password", "placeholder": "Password"}
-        ),
-    )
-
+    
     class Meta:
         model = UserModel
         fields = ['username', 'password']
@@ -67,27 +45,3 @@ class EditProfileForm(forms.ModelForm):
             "email",
             "profile_picture",
         ]
-
-        # widgets = {
-        #     "first_name": forms.TextInput(
-        #         attrs={"class": "form-control", "placeholder": "Enter your first name"}
-        #     ),
-        #     "last_name": forms.TextInput(
-        #         attrs={"class": "form-control", "placeholder": "Enter your last name"}
-        #     ),
-        #     "username": forms.TextInput(
-        #         attrs={"class": "form-control", "placeholder": "Enter your username"}
-        #     ),
-        #     "telephone_number": forms.TextInput(
-        #         attrs={"class": "form-control", "placeholder": "Enter your number"}
-        #     ),
-        #     "email": forms.EmailInput(
-        #         attrs={
-        #             "class": "form-control",
-        #             "placeholder": "Enter your email address",
-        #         }
-        #     ),
-        #     "profile_picture": forms.ClearableFileInput(
-        #         attrs={"class": "form-control"}
-        #     ),
-        # }
