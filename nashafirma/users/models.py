2243f7d015b9f4a6eaf5009323d590e20a3d27a9
VadimTrubay <vadnetvadnet@ukr.net>
2023-12-14 13:41:28 +0200

    Update models.py

--- nashafirma/users/models.py
+++ nashafirma/users/models.py
@@ -21,44 +21,32 @@ class SiteUser(AbstractUser):
     MIN_LEN_TELEPHONE_NUMBER = 7
 
     username = models.CharField(
-        null=False,
-        blank=False,
         unique=True,
         max_length=MAX_LEN_USERNAME,
         validators=(validators.MinLengthValidator(MIN_LEN_USERNAME),),
         default="",
     )
     email = models.EmailField(
-        null=False,
-        blank=False,
         unique=True,
         default="",
     )
     first_name = models.CharField(
-        null=True,
-        blank=True,
         max_length=MAX_LEN_FIRST_NAME,
         default="",
         validators=(validators.MinLengthValidator(MIN_LEN_FIRST_NAME),),
     )
     last_name = models.CharField(
-        null=True,
-        blank=True,
         max_length=MAX_LEN_SECOND_NAME,
         default="",
         validators=(validators.MinLengthValidator(MIN_LEN_SECOND_NAME),),
     )
     telephone_number = models.CharField(
-        null=True,
-        blank=True,
         max_length=MAX_LEN_TELEPHONE_NUMBER,
         validators=(validators.MinLengthValidator(MIN_LEN_TELEPHONE_NUMBER),),
     )
     profile_picture = models.ImageField(
         upload_to=user_directory_path,
         default="profile_pictures/profile_picture_default.jpg",
-        null=True,
-        blank=True,
         validators=[
             validate_file_size,
             validators.FileExtensionValidator(
