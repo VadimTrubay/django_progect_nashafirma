f8e878bf7fc5ff5955565439a8d2a895379add9e
VadimTrubay <vadnetvadnet@ukr.net>
2023-12-25 01:28:38 +0200

    Update settings.py

--- nashafirma/nashafirma/settings.py
+++ nashafirma/nashafirma/settings.py
@@ -63,23 +63,23 @@ WSGI_APPLICATION = "nashafirma.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": str(os.path.join(BASE_DIR, "db.sqlite3")),
-#     }
-# }
-"""Local PostgresSQL"""
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nashafirma',
-        'USER': 'nashafirma',
-        'PASSWORD': 'vad0101vad',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
+# """Local PostgresSQL"""
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'nashafirma',
+#         'USER': 'nashafirma',
+#         'PASSWORD': 'vad0101vad',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # """Local MySQL"""
 # DATABASES = {
