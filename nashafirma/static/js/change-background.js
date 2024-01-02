4997b891e536379b82c7bfc1b9c6239195e9bc5d
VadimTrubay <vadnetvadnet@ukr.net>
2024-01-02 22:10:59 +0200

    commit

--- nashafirma/static/js/change-background.js
+++ nashafirma/static/js/change-background.js
@@ -1,13 +1,13 @@
-(() => {
-// JavaScript код для изменения фона сайта
-function changeBackground() {
-    const container = document.querySelector('.container');
-    const backgrounds = ['background_3.jpg']
-    const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-    container.style.backgroundImage = `url('/static/images/${randomBackground}')`;
-}
+// (() => {
+// // JavaScript код для изменения фона сайта
+// function changeBackground() {
+//     const container = document.querySelector('.container');
+//     const backgrounds = ['background_3.jpg']
+//     const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+//     container.style.backgroundImage = `url('/static/images/${randomBackground}')`;
+// }
 
-// Вызов функции при смене страницы или событии, которое вы хотите использовать для изменения фона сайта
-// Например, вызов функции при загрузке страницы
-window.addEventListener('load', changeBackground);
-  })();
+// // Вызов функции при смене страницы или событии, которое вы хотите использовать для изменения фона сайта
+// // Например, вызов функции при загрузке страницы
+// window.addEventListener('load', changeBackground);
+//   })();
