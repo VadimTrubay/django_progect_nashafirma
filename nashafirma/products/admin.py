578c908488f89427f935ab3781b414f745cf8660
VadimTrubay <vadnetvadnet@ukr.net>
2024-01-14 19:19:14 +0200

    Update admin.py

--- nashafirma/products/admin.py
+++ nashafirma/products/admin.py
@@ -4,12 +4,12 @@ from .models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("product", "price", "id")
+    list_display = ("product", "price")
     list_totals = [('weight', lambda field: Coalesce(Sum(field), 0, output_field=FloatField()))]
     list_display_links = ("price", "price")
     search_fields = ("product", "price")
     list_editable = ("product",)
-    list_filter = ("product", "price", "id")
+    list_filter = ("product", "price")
 
 
 admin.site.register(Product, ProductAdmin)
