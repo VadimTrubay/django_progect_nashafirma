8a5aff81912c6da562b3fcdd0457c6365292052c
VadimTrubay <vadnetvadnet@ukr.net>
2024-01-18 21:30:06 +0200

    Update admin.py

--- nashafirma/users/admin.py
+++ nashafirma/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Feedback
+from .models import SiteUser
 
 
 @admin.register(Feedback)
@@ -10,3 +11,12 @@ class FeedbackAdmin(admin.ModelAdmin):
     """
     list_display = ('email', 'ip_address', 'user')
     list_display_links = ('email', 'ip_address')
+
+
+@admin.register(SiteUser)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели SiteUser
+    """
+    list_display = ('username', 'is_staff', 'email', 'first_name', 'last_name', 'telephone_number')
+    list_display_links = ('username', 'is_staff', 'email', 'first_name', 'last_name', 'telephone_number')
