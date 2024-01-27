31a79d3cd043b29d16413bb90031c05a0f12b13a
VadimTrubay <vadnetvadnet@ukr.net>
2024-01-27 14:07:16 +0200

    Create settings_.py

---
+++ nashafirma/nashafirma/settings_.py
@@ -0,0 +1,155 @@
+from pathlib import Path
+import os
+import environ
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")
+
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    "modeltranslation",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "admin_totals",
+    "captcha",
+    "orders.apps.OrdersConfig",
+    "products.apps.ProductsConfig",
+    "users.apps.UsersConfig",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.locale.LocaleMiddleware',
+]
+
+ROOT_URLCONF = "nashafirma.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["/home/nashafirmaod/django_progect_nashafirma/nashafirma/templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                'django.template.context_processors.i18n',
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "nashafirma.wsgi.application"
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),  # Set to your database host
+        'PORT': env('DATABASE_PORT'),  # Set to your database port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4'
+        },
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+LANGUAGES = (
+    ('uk', 'Ukrainian'),
+    ('ru', 'Russian'),
+    ('en', 'English'),
+)
+
+LANGUAGE_CODE = "uk"
+
+TIME_ZONE = "Europe/Kiev"
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+LOCALE_PATHS = ('locale',)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
+
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.SiteUser"
+
+LOGIN_URL = 'users'
+
+"""For Gmail"""
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_ADMIN = env('GMAIL_ADMIN')
+EMAIL_SERVER = env('GMAIL_SERVER')
+EMAIL_HOST = env('GMAIL_HOST')
+EMAIL_PORT = env('GMAIL_PORT')
+EMAIL_HOST_USER = env('GMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('GMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_STARTTLS = True
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+
+CAPTCHA_FONT_SIZE = 33
+CAPTCHA_LENGTH = 3
+# CAPTCHA_LETTER_ROTATION = -66, 66
